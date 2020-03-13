49326d3ff02bf75a499faa9e5c26a5a00bb95288
Park Min Soo <58577697+Park-MinSoo@users.noreply.github.com>
2020-03-13 17:47:39 +0900

    Create addprojectdata3.sql

---
+++ addprojectdata3.sql
@@ -0,0 +1,124 @@
+-- 남성 기타 아이템 정보 데이터 삽입
+insert into clothes(cl_pid,cl_type,cl_color,cl_tpo,cl_fabric,cl_uri)
+values('me001','가방(제품)','검은색','봄','가죽','http://70.12.115.167:8000/miniproject/resources/images/me001.png');
+
+insert into clothes(cl_pid,cl_type,cl_color,cl_tpo,cl_fabric,cl_uri)
+values('fme001','가방(착용)','검은색','봄','가죽','http://70.12.115.167:8000/miniproject/resources/images/fme001.png');
+
+insert into clothes(cl_pid,cl_type,cl_color,cl_tpo,cl_fabric,cl_uri)
+values('me004','캡모자(제품)','회색','봄','면','http://70.12.115.167:8000/miniproject/resources/images/me004.png');
+
+insert into clothes(cl_pid,cl_type,cl_color,cl_tpo,cl_fabric,cl_uri)
+values('fme004','캡모자(착용)','회색','봄','면','http://70.12.115.167:8000/miniproject/resources/images/fme004.png');
+
+insert into clothes(cl_pid,cl_type,cl_color,cl_tpo,cl_fabric,cl_uri)
+values('me006','모자(제품)','청색','가을','합성섬유','http://70.12.115.167:8000/miniproject/resources/images/me006.png');
+
+insert into clothes(cl_pid,cl_type,cl_color,cl_tpo,cl_fabric,cl_uri)
+values('fme006','모자(착용)','청색','가을','합성섬유','http://70.12.115.167:8000/miniproject/resources/images/fme006.png');
+
+insert into clothes(cl_pid,cl_type,cl_color,cl_tpo,cl_fabric,cl_uri)
+values('me007','가방(제품)','배색','가을','면','http://70.12.115.167:8000/miniproject/resources/images/me007.png');
+
+insert into clothes(cl_pid,cl_type,cl_color,cl_tpo,cl_fabric,cl_uri)
+values('fme007','가방(착용)','검은색','가을','면','http://70.12.115.167:8000/miniproject/resources/images/fme007.png');
+
+insert into clothes(cl_pid,cl_type,cl_color,cl_tpo,cl_fabric,cl_uri)
+values('me008','백팩(제품)','검은색','겨울','폴리에스테르','http://70.12.115.167:8000/miniproject/resources/images/me008.png');
+
+insert into clothes(cl_pid,cl_type,cl_color,cl_tpo,cl_fabric,cl_uri)
+values('fme008','백팩(착용)','검은색','겨울','폴리에스테르','http://70.12.115.167:8000/miniproject/resources/images/fme008.png');
+
+insert into clothes(cl_pid,cl_type,cl_color,cl_tpo,cl_fabric,cl_uri)
+values('me009','버킷햇(제품)','검은색','봄','면','http://70.12.115.167:8000/miniproject/resources/images/me009.png');
+
+insert into clothes(cl_pid,cl_type,cl_color,cl_tpo,cl_fabric,cl_uri)
+values('fme009','버킷햇(착용)','검은색','봄','면','http://70.12.115.167:8000/miniproject/resources/images/fme009.png');
+
+insert into clothes(cl_pid,cl_type,cl_color,cl_tpo,cl_fabric,cl_uri)
+values('me011','토트백(제품)','검은색','봄','가죽','http://70.12.115.167:8000/miniproject/resources/images/me011.png');
+
+insert into clothes(cl_pid,cl_type,cl_color,cl_tpo,cl_fabric,cl_uri)
+values('fme011','토트백(착용)','검은색','봄','가죽','http://70.12.115.167:8000/miniproject/resources/images/fme011.png');
+
+insert into clothes(cl_pid,cl_type,cl_color,cl_tpo,cl_fabric,cl_uri)
+values('me012','가방(제품)','카키색','가을','면','http://70.12.115.167:8000/miniproject/resources/images/me012.png');
+
+insert into clothes(cl_pid,cl_type,cl_color,cl_tpo,cl_fabric,cl_uri)
+values('fme012','가방(착용)','카키색','가을','면','http://70.12.115.167:8000/miniproject/resources/images/fme012.png');
+
+insert into clothes(cl_pid,cl_type,cl_color,cl_tpo,cl_fabric,cl_uri)
+values('me013','캡모자(제품)','인디핑크색','여름','면','http://70.12.115.167:8000/miniproject/resources/images/me013.png');
+
+insert into clothes(cl_pid,cl_type,cl_color,cl_tpo,cl_fabric,cl_uri)
+values('fme013','캡모자(착용)','인디핑크색','여름','면','http://70.12.115.167:8000/miniproject/resources/images/fme013.png');
+
+insert into clothes(cl_pid,cl_type,cl_color,cl_tpo,cl_fabric,cl_uri)
+values('me014','가방(제품)','베이지색','여름','면','http://70.12.115.167:8000/miniproject/resources/images/me014.png');
+
+insert into clothes(cl_pid,cl_type,cl_color,cl_tpo,cl_fabric,cl_uri)
+values('fme014','가방(착용)','베이지색','여름','면','http://70.12.115.167:8000/miniproject/resources/images/fme014.png');
+
+insert into clothes(cl_pid,cl_type,cl_color,cl_tpo,cl_fabric,cl_uri)
+values('me015','비니(제품)','검은색','겨울','털','http://70.12.115.167:8000/miniproject/resources/images/me015.png');
+
+insert into clothes(cl_pid,cl_type,cl_color,cl_tpo,cl_fabric,cl_uri)
+values('fme015','비니(착용)','검은색','겨울','털','http://70.12.115.167:8000/miniproject/resources/images/fme015.png');
+
+
+-- 남성 바지 아이템 정보 데이터 삽입
+insert into clothes(cl_pid,cl_type,cl_color,cl_tpo,cl_fabric,cl_uri)
+values('mp001','반바지(제품)','검은색','여름','면','http://70.12.115.167:8000/miniproject/resources/images/mp001.png');
+
+insert into clothes(cl_pid,cl_type,cl_color,cl_tpo,cl_fabric,cl_uri)
+values('fmp001','반바지(착용)','검은색','여름','면','http://70.12.115.167:8000/miniproject/resources/images/fmp001.png');
+
+insert into clothes(cl_pid,cl_type,cl_color,cl_tpo,cl_fabric,cl_uri)
+values('mp002','면바지(제품)','검은색','봄','면','http://70.12.115.167:8000/miniproject/resources/images/mp002.png');
+
+insert into clothes(cl_pid,cl_type,cl_color,cl_tpo,cl_fabric,cl_uri)
+values('fmp002','면바지(착용)','검은색','봄','면','http://70.12.115.167:8000/miniproject/resources/images/fmp002.png');
+
+insert into clothes(cl_pid,cl_type,cl_color,cl_tpo,cl_fabric,cl_uri)
+values('mp003','면바지(제품)','베이지색','가을','면','http://70.12.115.167:8000/miniproject/resources/images/mp003.png');
+
+insert into clothes(cl_pid,cl_type,cl_color,cl_tpo,cl_fabric,cl_uri)
+values('fmp003','면바지(착용)','베이지색','가을','면','http://70.12.115.167:8000/miniproject/resources/images/fmp003.png');
+
+insert into clothes(cl_pid,cl_type,cl_color,cl_tpo,cl_fabric,cl_uri)
+values('mp004','조거진(제품)','검은색','봄','면','http://70.12.115.167:8000/miniproject/resources/images/mp004.png');
+
+insert into clothes(cl_pid,cl_type,cl_color,cl_tpo,cl_fabric,cl_uri)
+values('fmp004','조거진(착용)','검은색','봄','면','http://70.12.115.167:8000/miniproject/resources/images/fmp004.png');
+
+insert into clothes(cl_pid,cl_type,cl_color,cl_tpo,cl_fabric,cl_uri)
+values('mp005','디스진(제품)','청색','봄','청','http://70.12.115.167:8000/miniproject/resources/images/mp005.png');
+
+insert into clothes(cl_pid,cl_type,cl_color,cl_tpo,cl_fabric,cl_uri)
+values('fmp005','디스진(착용)','청색','봄','청','http://70.12.115.167:8000/miniproject/resources/images/fmp005.png');
+
+insert into clothes(cl_pid,cl_type,cl_color,cl_tpo,cl_fabric,cl_uri)
+values('mp006','반바지(제품)','청색','여름','청','http://70.12.115.167:8000/miniproject/resources/images/mp006.png');
+
+insert into clothes(cl_pid,cl_type,cl_color,cl_tpo,cl_fabric,cl_uri)
+values('fmp006','반바지(착용)','청색','여름','청','http://70.12.115.167:8000/miniproject/resources/images/fmp006.png');
+
+insert into clothes(cl_pid,cl_type,cl_color,cl_tpo,cl_fabric,cl_uri)
+values('mp007','청바지(제품)','청색','겨울','청','http://70.12.115.167:8000/miniproject/resources/images/mp007.png');
+
+insert into clothes(cl_pid,cl_type,cl_color,cl_tpo,cl_fabric,cl_uri)
+values('fmp007','청바지(착용)','청색','겨울','청','http://70.12.115.167:8000/miniproject/resources/images/fmp007.png');
+
+insert into clothes(cl_pid,cl_type,cl_color,cl_tpo,cl_fabric,cl_uri)
+values('mp008','카고진(제품)','회색','여름','면','http://70.12.115.167:8000/miniproject/resources/images/mp008.png');
+
+insert into clothes(cl_pid,cl_type,cl_color,cl_tpo,cl_fabric,cl_uri)
+values('fmp008','카고진(착용)','회색','여름','면','http://70.12.115.167:8000/miniproject/resources/images/fmp008.png');
+
+insert into clothes(cl_pid,cl_type,cl_color,cl_tpo,cl_fabric,cl_uri)
+values('mp009','면바지(제품)','흰색','가을','면','http://70.12.115.167:8000/miniproject/resources/images/mp009.png');
+
+insert into clothes(cl_pid,cl_type,cl_color,cl_tpo,cl_fabric,cl_uri)
+values('fmp009','면바지(착용)','흰색','가을','면','http://70.12.115.167:8000/miniproject/resources/images/fmp009.png');
+
+commit;
